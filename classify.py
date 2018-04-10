c9b98a94b7a233f6ba9984f804f1f7542414af96
kekeller <kekeller@ncsu.edu>
2018-04-10 13:39:52 +0200

    updated classify

--- classify.py
+++ classify.py
@@ -1,6 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 import glob
-from skimage import io
+from skimage import io,img_as_uint
 from sklearn.neural_network import MLPClassifier
 import numpy as np
 
@@ -41,6 +41,7 @@ def read_data(path):
 
 	return train,label
 
+# return a black and white mask
 def convert_label_mask(labels):
 	mask = []
 	for label in labels:
@@ -62,32 +63,87 @@ def convert_label_mask(labels):
 					x = np.array(x)
 					row[indx] = x
 		mask.append(m.flatten())
+		io.imsave("mask.tif",m.reshape(250,250,3))
 	return mask
 
-train,label = read_data("./images")
-mask = convert_label_mask(label)
+#train,label = read_data("./images")
+#mask = convert_label_mask(label)
 
-train_validate,label_validate = read_data("./predict")
-mask_validate = convert_label_mask(label_validate)
+#train_validate,label_validate = read_data("./predict")
+#mask_validate = convert_label_mask(label_validate)
 
-print(len(train_validate))
-
-clf = RandomForestClassifier(verbose=1)
-
-clf.fit(train, mask)
-pred = clf.predict(train_validate)
-pred = np.array(pred)
-pred= pred.reshape(250,250,3)
-
-print(np.mean(pred))
-for row in pred:
-	for indx, pixel in enumerate(row):
-		x = []
-		if np.mean(pixel) > 0:
-			x = [np.uint8(pixel[0]),np.uint8(pixel[1]),np.uint8(pixel[2])]
+def fake_images_green():
+	im = np.linspace(0, 1., (250*250*3)).reshape(250,250,3)
+	for row in im:
+		for indx, pixel in enumerate(row):
+			x = (0,1.0,0.255) # set to green
 			x = np.array(x)
 			row[indx] = x
 
-io.imsave("out.tif",mask[0].reshape(250,250,3))
-io.imsave("out1.tif",train_validate[0].reshape(250,250,3))
-io.imsave("out2.tif",pred)
+	im = img_as_uint(im)
+	return im
+
+def fake_images_white():
+	im = np.linspace(0, 1., (250*250*3)).reshape(250,250,3)
+	for row in im:
+		for indx, pixel in enumerate(row):
+			x = (1,1,1) 
+			x = np.array(x)
+			row[indx] = x
+
+	im = img_as_uint(im)
+	return im
+
+def fake_images_mask():
+	im = np.linspace(0, 1., (250*250*3)).reshape(250,250,3)
+	for row in im:
+		for indx, pixel in enumerate(row):
+			x = (0,0,0) 
+			x = np.array(x)
+			row[indx] = x
+
+	im = img_as_uint(im)
+	return im
+
+def fake_data():
+	green = fake_images_green()
+	white = fake_images_white()
+	mask = fake_images_mask()
+	io.imsave("green.tif",green)
+	io.imsave("white.tif",white)
+	return green,white,mask
+
+green,white,mask = fake_data()
+x = []
+x.append(green.flatten())
+x.append(white.flatten())
+y = []
+y.append(mask.flatten())
+y.append(white.flatten())
+test = []
+test.append(white.flatten())
+
+clf = RandomForestClassifier(verbose=1)
+clf.fit(x,y)
+print(clf)
+pred = clf.predict(test)
+print(pred)
+
+# clf.fit(train, mask)
+# pred = clf.predict(train_validate)
+# pred = np.array(pred)
+# pred= pred.reshape(250,250,3)
+
+# print(np.mean(pred))
+# x = []
+# for row in pred:
+# 	for indx, pixel in enumerate(row):
+# 			x = [np.uint8(pixel[0]),np.uint8(pixel[1]),np.uint8(pixel[2])]
+# 			x = np.array(x)
+# 			row[indx] = x
+
+# pred = pred.astype(int)
+# print(np.mean(pred))
+
+# io.imsave("training_image.tif",train_validate[0].reshape(250,250,3))
+# io.imsave("output.tif",pred)
